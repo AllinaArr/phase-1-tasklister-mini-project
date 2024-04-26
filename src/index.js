33e84093d523cdfc933b028c99aebc2bd9376f4e
Alina Rudanets <82982601+AllinaArr@users.noreply.github.com>
2024-04-26 08:42:05 -0400

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const todo = document.querySelector("form");
+
+  todo.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // const submitToDo = document.querySelector("#new-task-description");
+
+    const inputValue = e.target["new-task-description"].value;
+
+    const list = document.querySelector("#tasks");
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.textContent = "x";
+    btn.addEventListener("click", () => li.remove());
+    li.textContent = inputValue;
+
+    list.append(li);
+    li.appendChild(btn);
+  });
 });
